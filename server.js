d12b33e3365e56cf800b09077b94431e0f210e02
shivanshi <shivanshi.saxena2@gmail.com>
2023-11-05 19:08:00 +0530

    cleaningup

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const dotenv= require('dotenv');
-dotenv.config({path: './config.env'}); //CANT READ PROCESS IN AP BECAUSE IT WASNT CONFIGURED THATS WHY ITS UP
-
+const dotenv = require('dotenv');
 const app = require('./app');
+dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-const port= process.env.PORT || 3000;
-app.listen(port,() => {
-    console.log(`App running on port ${port}....`);
-})
+
+mongoose
+    // .connect(process.env.DATABASE_LOCAL,{
+    .connect(DB, {
+        useNewUrlParser : true,
+        useCreateIndex : true,
+        useFindAndModify : false
+    }).then(() => console.log("DB Connected !!"));
+
+
+//instance of the tour model
+// const testTour = new Tour({
+//     name : "The Park Camper",
+//     price : 400 
+// })
+// testTour.save().then( doc => {
+//     console.log(doc);
+// }).catch( err => {
+//     console.log("ERROR!!", err)
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
